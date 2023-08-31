9646ccf927cd4643797a562af991cbf98aa75b8c
Tony Jara <340studiospy@gmail.com>
2023-08-31 00:36:21 -0300

    fixed title

--- src/components/DynamicTables/Columns/AdminLogsColumn.tsx
+++ src/components/DynamicTables/Columns/AdminLogsColumn.tsx
@@ -1,23 +1,11 @@
 import { createColumnHelper } from "@tanstack/react-table";
-import DateCell from "@/components/DynamicTables/DynamicCells/DateCell";
 import TextCell from "@/components/DynamicTables/DynamicCells/TextCell";
-import { Episode, Logs } from "@prisma/client";
+import { Logs } from "@prisma/client";
 import MillisecondsCell from "../DynamicCells/MilliSecondsCell";
 
 const columnHelper = createColumnHelper<Logs>();
 
 export const adminLogsColumn = () => [
-  /* columnHelper.accessor("episodeNumber", { */
-  /*   cell: (x) => `# ${x.getValue()}` ?? "-", */
-  /*   header: "№", */
-  /*   sortingFn: "datetime", */
-  /* }), */
-  /* columnHelper.accessor("releaseDate", { */
-  /*   cell: (x) => */
-  /*     x.getValue() ? <DateCell date={x.getValue() ?? new Date()} /> : "-", */
-  /*   header: "Release Date", */
-  /*   sortingFn: "datetime", */
-  /* }), */
   columnHelper.accessor("createdAt", {
     cell: (x) => <MillisecondsCell date={x.getValue()} />,
     header: "Created At",
@@ -40,11 +28,4 @@ export const adminLogsColumn = () => [
     header: "Event Id",
     sortingFn: "text",
   }),
-
-  /* columnHelper.accessor("status", { */
-  /*   cell: (x) => <TextCell text={x.getValue()} />, */
-  /*   header: "Status", */
-  /*   sortingFn: "text", */
-  /* }), */
-  //TODO add small audio player
 ];
