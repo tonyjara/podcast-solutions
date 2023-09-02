e31a52fe523683a406079adcef72efaea43b6bea
Tony Jara <340studiospy@gmail.com>
2023-09-02 19:19:45 -0300

    added transcription and guid to episode tags

--- src/components/XML/ItunesFeedBuilder.ts
+++ src/components/XML/ItunesFeedBuilder.ts
@@ -76,6 +76,8 @@ export const generatePodcastRssFeed = async (podcast: PodcastTypeForFeed) => {
           /* { "podcast:transcript": episode.transcription }, */ //Needs a url
           {
             "itunes:image": episode.imageUrl,
+            "podcast:transcript": episode.transcription,
+            "podcast:guid": episode.id,
           },
         ],
       });
